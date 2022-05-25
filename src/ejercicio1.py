212ed5e5e4e36233a3fc0927544a779e44060a50
l o l i <105023532+lolafioramanti@users.noreply.github.com>
2022-05-25 17:49:25 -0300

    Agregar ejercicio1.py y su otra versión
    Corrección error tipográfico (ejercicio2.py por ejercicio1.py)
    Se agregó la primera versión del programa.

--- src/ejercicio1.py
+++ src/ejercicio1.py
@@ -35,3 +35,29 @@ def principal():
 if __name__ == "__main__":
     principal()
     
+# Primero había entendido que el programa debía convertir grados, pero dependiendo lo que quisiera el usuario, así que había programado esto:
+# PRECONDICIONES: ingresar un número y el grado a convertir.
+# POSTCONDICIONES: obtener la conversión deseada.
+#def convertir_a_fahrenheit(centigrados):
+#    fahrenheit = (centigrados * 1.8 ) + 32
+#    resultado = fahrenheit
+#    return resultado
+#def convertir_a_centigrados(fahrenheit):
+#    centigrados = (fahrenheit - 32) / 1.8
+#    resultado = centigrados
+#    return resultado
+#def principal():
+#    """
+#    Conversión de grados centígrados a fahrenheit, y viceversa.
+#    """
+#    num = int(input("Ingrese 1 para calcular de grados Centígrados a Fahrenheit. \nIngrese cualquier otro número para calcular de grados Fahrenheit a Centígrados.  "))
+#    if num != 1:
+#        grados = int(input("Ingrese los grados a convertir: "))
+#        conversion = convertir_a_centigrados(grados)
+#        print(f"{conversion}ºC")
+#    else:
+#        grados = int(input("Ingrese los grados a convertir: "))
+#        conversion = convertir_a_fahrenheit(grados)
+#        print(f"{conversion}ºF")
+#if __name__ == "__main__":
+#    principal()
