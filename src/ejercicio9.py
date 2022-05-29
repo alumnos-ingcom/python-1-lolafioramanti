ba298ad7d9e10113eb0ee1ab304fc78a64200e8d
l o l i <105023532+lolafioramanti@users.noreply.github.com>
2022-05-28 21:45:17 -0300

    Agregué ejercicio9.py

---
+++ src/ejercicio9.py
@@ -0,0 +1,34 @@
+################
+# Lola Agustina Fioramanti - @lolafioramanti
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+"""
+Escribir una función que retorne una tuple con factores primos de un numero entero positivo.
+PRECONDICION: El valor de entrada debe ser un entero positivo.
+POSTCONDICION: El valor de salida debe ser en formato tuple.
+"""
+
+def factores_primos(numero):
+    '''Esta función se encarga de encontrar los factores primos de un número.'''
+    divisor = 2
+    factores = []
+    while numero > 1:
+        if numero % divisor != 0:
+            divisor += 1
+        else:
+            numero = numero // divisor
+            factores.append(divisor)
+    tupla = tuple(factores)
+    return tupla
+        
+def principal():
+    '''Esta función se encarga de interactuar con el usuario.
+    Pide un número como entrada, lo conecta con la función, e imprime como salida una tuple.'''
+    numero = int(input("Ingrese un número: "))
+    factores = factores_primos(numero)
+    print(f"Los factores primos son {factores}")
+
+if __name__ == "__main__":
+    principal()
+    
