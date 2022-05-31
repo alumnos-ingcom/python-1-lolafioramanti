cce8b60f1e20432be4131ede0d5efe727dabd034
l o l i <105023532+lolafioramanti@users.noreply.github.com>
2022-05-31 19:44:40 -0300

    Arreglo de valores iguales.
    Se pueden ingresar valores iguales.

--- src/ejercicio6.py
+++ src/ejercicio6.py
@@ -22,33 +22,33 @@ POSTCONDICIÓN: Los valores deben ser enteros ordenados de menor a mayor y vicev
 
 def ordenar_mayor_a_menor(uno, dos, tres):
     ''' Esta función ordena los números de mayor a menor y los guarda en una lista. '''
-    if uno > dos and dos > tres :
+    if uno >= dos and dos >= tres :
         valorMm = [uno, dos, tres]
-    elif uno > tres and tres > dos :
+    elif uno >= tres and tres >= dos :
         valorMm = [uno, tres, dos]
-    elif dos > uno and uno > tres :
+    elif dos >= uno and uno >= tres :
         valorMm = [dos, uno, tres]
-    elif dos > tres and tres > uno :
+    elif dos >= tres and tres >= uno :
         valorMm = [dos, tres, uno]
-    elif tres > uno and uno > dos :
+    elif tres >= uno and uno >= dos :
         valorMm = [tres, uno, dos]
-    elif tres > dos and dos > uno :
+    elif tres >= dos and dos >= uno :
         valorMm = [tres, dos, uno]
     valorMm = tuple(valorMm)
     return valorMm
 def ordenar_menor_a_mayor(uno, dos, tres):
     ''' Esta función ordena los números de menor a mayor y los guarda en una lista. '''
-    if uno < dos and dos < tres :
+    if uno <= dos and dos <= tres :
         valormM = [uno, dos, tres]
-    elif uno < tres and tres < dos :
+    elif uno <= tres and tres <= dos :
         valormM = [uno, tres, dos]
-    elif dos < uno and uno < tres :
+    elif dos <= uno and uno <= tres :
         valormM = [dos, uno, tres]
-    elif dos < tres and tres < uno :
+    elif dos <= tres and tres <= uno :
         valormM = [dos, tres, uno]
-    elif tres < uno and uno < dos :
+    elif tres <= uno and uno <= dos :
         valormM = [tres, uno, dos]
-    elif tres < dos and dos < uno :
+    elif tres <= dos and dos <= uno :
         valormM = [tres, dos, uno]
     valormM = tuple(valormM)
     return valormM
