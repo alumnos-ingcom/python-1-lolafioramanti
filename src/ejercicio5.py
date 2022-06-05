197da605028f7f78a23aba293974d295b329adf9
l o l i <105023532+lolafioramanti@users.noreply.github.com>
2022-06-04 23:46:33 -0300

    Corrección
    Este es el archivo que debí subir, lo confundí con el original que había hecho.

--- src/ejercicio5.py
+++ src/ejercicio5.py
@@ -9,33 +9,23 @@ PRECONDICIONES: Los valores de entrada deben ser dos números enteros.
 POSTCONDIONES: El valor de salida deben un mensaje con dos números, siendo que la cuenta fue exitosa.
 """
 
-def valor_absoluto(dividendo, divisor):
-    ''' Función que cambia los numeros negativos por su valor absoluto, guarda el signo en una variable y el número original. '''
-    if dividendo < 0 and divisor < 0:
-        orig_dividendo = dividendo
-        dividendo = abs(dividendo)
-        orig_divisor = divisor
-        divisor = abs(divisor)
-        signo = 1
-    elif divisor < 0:
-        orig_divisor = divisor
-        orig_dividendo = dividendo
-        divisor = abs(divisor)
-        signo = -1
-    elif dividendo < 0:
-        orig_dividendo = dividendo
-        orig_divisor = divisor
-        dividendo = abs(dividendo)
-        signo = -1
-    elif dividendo > 0 and divisor > 0:
-        orig_dividendo = dividendo
-        orig_divisor = divisor
-        signo = 1
-    return dividendo, divisor, orig_dividendo, orig_divisor, signo
+def signos(numero):
+    if numero != 0:
+        suma = numero + abs(numero)
+        resta = numero - abs(numero)
+        if resta == 0:
+            resultado = 1
+        else:
+            resultado = -1
+    else:
+        resultado = 0
+    return resultado
 
 def division_lenta(dividendo, divisor, signo):
     ''' Función que realiza una resta sucesiva hasta obtener el cociente y resto de dos números. También se multiplica el cociente por el signo original de los valores si es necesario. '''
     cociente = 0
+    dividendo = abs(dividendo)
+    divisor = abs(divisor)
     if divisor != 0:
         if dividendo >= divisor:
             while (dividendo - divisor) >= 0:
@@ -47,13 +37,18 @@ def division_lenta(dividendo, divisor, signo):
     else:
         print("No es posible realizar el cálculo con divisor 0.")
     cociente *= signo
-    return cociente, resto   
+    return cociente, resto
+
 def principal():
     ''' Función con la que interactúa el usuario. Obtiene los valores de entrada, ejecuta las dos funciones definidas y se encarga de imprimir la salida. '''
     dividendo = int(input("Ingrese el dividendo: "))
+    numero = dividendo
+    signo_dividendo = signos(numero)
     divisor = int(input("Ingrese el divisor: "))
-    dividendo, divisor, original_dividendo, original_divisor, signo = valor_absoluto(dividendo, divisor)
-    cociente_cuenta, resto_cuenta = division_lenta(dividendo, divisor, signo)
-    print(f"La cuenta {original_dividendo} / {original_divisor} tiene como cociente {cociente_cuenta} y resto {resto_cuenta}")
+    numero = divisor
+    signo_divisor = signos(numero)
+    signo = signo_divisor * signo_dividendo
+    cociente, resto = division_lenta(dividendo, divisor, signo)
+    print(f"Cociente {cociente} y resto {resto}")
 if __name__ == "__main__":
     principal()
